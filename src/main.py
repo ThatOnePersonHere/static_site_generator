f98cd1dee41bc44c36b856e8aec2e786f616d4ed
ThatOnePersonHere <JJSonsteng@gmail.com>
2025-06-11 13:57:13 -0700

    changing template and main

--- src/main.py
+++ src/main.py
@@ -237,7 +237,6 @@ def generate_page(from_path, template_path, dest_path,basepath):
     loc_from = source_cont.read()
     loc_temp = template.read()
     with open(dest_path, 'w', opener=opener) as l:
-        console.log(sys.argv[1])
         page_output = loc_temp.replace('{{ Content }}',markdown_to_html_node(loc_from).to_html())
         page_output = page_output.replace('{{ Title }}',extract_title(loc_from))
         page_output = page_output.replace('src="./',f'src="{sys.argv[1]}/')
